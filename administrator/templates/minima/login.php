067c460e64515af402a43c17c507bdef1d4e2299
Rouven Wessling <me@rouvenwessling.de>
2011-02-20 23:50:11 +0100

    No need to declare the charset twice.

--- administrator/templates/minima/login.php
+++ administrator/templates/minima/login.php
@@ -25,7 +25,6 @@ $app = JFactory::getApplication();
 
 <head>
 
-    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
