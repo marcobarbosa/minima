1894f701f4c9a9bc0a2c31bc05fb41e16811a415
Marco Barbosa <iam@marcobarbosa.com>
2011-04-03 17:13:37 +0200

    renamed installation folder

---
+++ _installation/index.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @version		$Id: index.php 20806 2011-02-21 19:44:59Z dextercowley $
+ * @package		Joomla.Installation
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// PHP 5 check
+if (version_compare(PHP_VERSION, '5.2.4', '<')) {
+	die('Your host needs to use PHP 5.2.4 or higher to run Joomla 1.6.');
+}
+
+/**
+ * Constant that is checked in included files to prevent direct access.
+ */
+define('_JEXEC', 1);
+
+/**
+ * Constant that defines the base path of the installed Joomla site.
+ */
+define('JPATH_BASE', dirname(__FILE__));
+
+/**
+ * Shortcut for the directory separator character.
+ */
+define('DS', DIRECTORY_SEPARATOR);
+
+// Set path constants.
+$parts = explode(DS, JPATH_BASE);
+array_pop($parts);
+
+define('JPATH_ROOT',			implode(DS, $parts));
+define('JPATH_SITE',			JPATH_ROOT);
+define('JPATH_CONFIGURATION',	JPATH_ROOT);
+define('JPATH_ADMINISTRATOR',	JPATH_ROOT.DS.'administrator');
+define('JPATH_LIBRARIES',		JPATH_ROOT.DS.'libraries');
+define('JPATH_PLUGINS',			JPATH_ROOT.DS.'plugins');
+define('JPATH_INSTALLATION',	JPATH_ROOT.DS.'installation');
+define('JPATH_THEMES',			JPATH_BASE);
+define('JPATH_CACHE',			JPATH_ROOT.DS.'cache');
+
+/*
+ * Joomla system checks.
+ */
+error_reporting(E_ALL);
+@ini_set('magic_quotes_runtime', 0);
+@ini_set('zend.ze1_compatibility_mode', '0');
+
+/*
+ * Check for existing configuration file.
+ */
+if (file_exists(JPATH_CONFIGURATION.'/configuration.php') && (filesize(JPATH_CONFIGURATION.'/configuration.php') > 10) && !file_exists(JPATH_INSTALLATION.'/index.php')) {
+	header('Location: ../index.php');
+	exit();
+}
+
+/*
+ * Joomla system startup
+ */
+
+// Bootstrap the Joomla Framework.
+require_once JPATH_LIBRARIES.'/joomla/import.php';
+
+// Joomla library imports.
+jimport('joomla.database.table');
+jimport('joomla.user.user');
+jimport('joomla.environment.uri');
+jimport('joomla.html.parameter');
+jimport('joomla.utilities.utility');
+jimport('joomla.language.language');
+jimport('joomla.utilities.string');
+
+// Create the application object.
+$app = JFactory::getApplication('installation');
+
+// Initialise the application.
+$app->initialise();
+
+// Render the document.
+$app->render();
+
+// Return the response.
+echo $app;
---
+++ _installation/views/license/view.html.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @version		$Id: view.html.php 20196 2011-01-09 02:40:25Z ian $
+ * @package		Joomla.Installation
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.view');
+jimport('joomla.html.html');
+
+/**
+ * The HTML Joomla Core License View
+ *
+ * @package		Joomla.Installation
+ * @since		1.6
+ */
+class JInstallationViewLicense extends JView
+{
+	/**
+	 * Display the view
+	 *
+	 * @access	public
+	 */
+	function display($tpl = null)
+	{
+		$state = $this->get('State');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			JError::raiseError(500, implode("\n", $errors));
+			return false;
+		}
+
+		$this->assignRef('state', $state);
+
+		parent::display($tpl);
+	}
+}
