11170bd05e63875ffaa8e1a66f15838bcc493c8a
Marco Barbosa <iam@marcobarbosa.com>
2011-04-11 19:55:35 +0200

    callign minified files for demo

--- administrator/templates/minima/cpanel.php
+++ administrator/templates/minima/cpanel.php
@@ -130,7 +130,7 @@ $hidemainmenu = JRequest::getInt('hidemainmenu');
     </footer>
     <script>
         head.js(
-            {minima: "templates/<?php echo $this->template ?>/js/minima.js"}            
+            {minima: "templates/<?php echo $this->template ?>/js/minima.min.js"}            
         , function() {
             // all done            
             $(document.body).addClass('ready');            
--- administrator/templates/minima/index.php
+++ administrator/templates/minima/index.php
@@ -150,8 +150,8 @@ $hidemainmenu = JRequest::getInt('hidemainmenu');
     </footer>
     <script>
         head.js(
-            {minima: "templates/<?php echo $this->template ?>/js/minima.js"},
-            {fixes: "templates/<?php echo $this->template ?>/js/libs/minima.fixes.js"}
+            {minima: "templates/<?php echo $this->template ?>/js/minima.min.js"},
+            {fixes: "templates/<?php echo $this->template ?>/js/libs/minima.fixes.min.js"}
         , function() {
             // all done            
             $(document.body).addClass('ready');            
